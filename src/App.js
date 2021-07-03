fa4beb523ea37f23dfa6d8275a6196aa44ad2681
Wone <lochlainn.gadault@gmail.com>
2021-07-03 22:44:52 +0200

    contact list with map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import Contact from "./components/Contact";
+import ContactList from "./components/ContactList";
 
 function App() {
   return (
     <div className="App">
-      <Contact
-        name="Anita Sutton"
-        avatar="https://randomuser.me/api/portraits/women/27.jpg"
-        online
-      />
-      <Contact
-        name="Corey Day"
-        avatar="https://randomuser.me/api/portraits/men/38.jpg"
-        offline
-      />
-      <Contact
-        name="Carter Stone"
-        avatar="https://randomuser.me/api/portraits/men/41.jpg"
-        online
-      />
+      <ContactList />
     </div>
   );
 }
